d56e44bd5dcd8b8dea371a9c7f2b1b214027e9b5
harshpandya43 <pandyaharsh43@gmail.com>
2018-07-28 12:50:35 -0500

    Validations

--- includes/handlers/registerhandler.php
+++ includes/handlers/registerhandler.php
@@ -17,24 +17,46 @@
         $inputText = ucfirst(strtolower($inputText));
         return $inputText;
     }
+
+    function validateUserName($name){
+
+    }
+
+    function validateFirstName($firstName){
+        
+    }
+
+    function validateLastName($lastName){
+        
+    }
+
+    function validatePasswords($password, $confirmPassword){
+        
+    }
+
+    function validateEmails($email, $confirmEmail){
+        
+    }
    
 
     //Register Button
     if(isset($_POST['registerButton'])){
         // echo "Register button is pressed";
         $userName = sanitizeFormUserName($_POST['userName']);
-
-        $firstName = sanitizeFormName($_POST['firstName']);      
-
-        $lastName = sanitizeFormName($_POST['lastName']);        
-
-        $email = sanitizeFormUserName($_POST['email']);
-        
-        $confirmEmail = sanitizeFormUserName($_POST['confirmEmail']);
-        
+        $firstName = sanitizeFormName($_POST['firstName']);
+        $lastName = sanitizeFormName($_POST['lastName']);
+        $email = sanitizeFormUserName($_POST['email']);        
+        $confirmEmail = sanitizeFormUserName($_POST['confirmEmail']);        
         $password = sanitizeFormPassword($_POST['password']);       
-
         $confirmPassword = sanitizeFormPassword($_POST['confirmPassword']);
         
+
+        validateUserName($userName);
+        validateFirstName($firstName);
+        validateLastName($lastName);
+        validateEmails($email, $confirmEmail);
+        validatePasswords($password, $confirmPassword);
+
+
     }
 ?>
