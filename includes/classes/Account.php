30023dfae31aae0cb2da6e8843a39799750d697b
harshpandya43 <pandyaharsh43@gmail.com>
2018-07-28 23:33:03 -0500

    Validate email and usernames

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -23,19 +23,35 @@
             }          
         
             private function validateFirstName($firstName){
-                
+                if(strlen($firstName)>25 || strlen($firstName)<2){
+                    array_push($this->errorArray, "Your firstname must be between 2 and 25 characters");
+                    return;
+                }
             }
         
             private function validateLastName($lastName){
-                
+                if(strlen($lastName)>25 || strlen($lastName)<2){
+                    array_push($this->errorArray, "Your lastname must be between 2 and 25 characters");
+                    return;
+                }
             }
         
             private function validatePasswords($password, $confirmPassword){
-                
+            
             }
         
             private function validateEmails($email, $confirmEmail){
-                
+                if($email != $confirmEmail){
+                    array_push($this->errorArray, "Your email don't match.");
+                    return;
+                }
+
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                    array_push($this->errorArray,"Please check the format of email.");
+                    return;
+                }
+
+                //TODO : Check that username is already being used.
             }
            
         
